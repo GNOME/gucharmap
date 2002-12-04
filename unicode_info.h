0a56ce07440cc253979bd0cdda05880fb4e70d53
Noah Levitt <nlevitt@src.gnome.org>
2002-12-04 22:13:51 +0000

    put the east asian info in there, wrote functions to retrieve (not used yet)

--- unicode_info.h
+++ unicode_info.h
@@ -28,44 +28,16 @@
 extern "C" {
 #endif /* __cplusplus */
 
-typedef enum {
-  UNICODE_CATEGORY_Lu = 101, /* Letter, Uppercase */
-  UNICODE_CATEGORY_Ll, /* Letter, Lowercase */
-  UNICODE_CATEGORY_Lt, /* Letter, Titlecase */
-  UNICODE_CATEGORY_Lm, /* Letter, Modifier */
-  UNICODE_CATEGORY_Lo, /* Letter, Other */
-  UNICODE_CATEGORY_Mn, /* Mark, Non-Spacing */
-  UNICODE_CATEGORY_Mc, /* Mark, Spacing Combining */
-  UNICODE_CATEGORY_Me, /* Mark, Enclosing */
-  UNICODE_CATEGORY_Nd, /* Number, Decimal Digit */
-  UNICODE_CATEGORY_Nl, /* Number, Letter */
-  UNICODE_CATEGORY_No, /* Number, Other */
-  UNICODE_CATEGORY_Pc, /* Punctuation, Connector */
-  UNICODE_CATEGORY_Pd, /* Punctuation, Dash */
-  UNICODE_CATEGORY_Ps, /* Punctuation, Open */
-  UNICODE_CATEGORY_Pe, /* Punctuation, Close */
-  UNICODE_CATEGORY_Pi, /* Punctuation, Initial quote */
-  UNICODE_CATEGORY_Pf, /* Punctuation, Final quote  */
-  UNICODE_CATEGORY_Po, /* Punctuation, Other */
-  UNICODE_CATEGORY_Sm, /* Symbol, Math */
-  UNICODE_CATEGORY_Sc, /* Symbol, Currency */
-  UNICODE_CATEGORY_Sk, /* Symbol, Modifier */
-  UNICODE_CATEGORY_So, /* Symbol, Other */
-  UNICODE_CATEGORY_Zs, /* Separator, Space */
-  UNICODE_CATEGORY_Zl, /* Separator, Line */
-  UNICODE_CATEGORY_Zp, /* Separator, Paragraph */
-  UNICODE_CATEGORY_Cc, /* Other, Control */
-  UNICODE_CATEGORY_Cf, /* Other, Format */
-  UNICODE_CATEGORY_Cs, /* Other, Surrogate */
-  UNICODE_CATEGORY_Co, /* Other, Private Use */
-  UNICODE_CATEGORY_Cn, /* Other, Not Assigned */
-} unicode_category_t;
-
-
 /* return values are read-only */
 gchar * get_unicode_name (gunichar uc);
 gchar * get_unicode_category_name (gunichar uc);
-
+gchar * get_kDefinition (gunichar uc);
+gchar * get_kCantonese (gunichar uc);
+gchar * get_kMandarin (gunichar uc);
+gchar * get_kTang (gunichar uc);
+gchar * get_kKorean (gunichar uc);
+gchar * get_kJapeneseKun (gunichar uc);
+gchar * get_kJapaneseOn (gunichar uc);
 
 #ifdef __cplusplus
 }
