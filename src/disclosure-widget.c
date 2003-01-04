bced623fbfb499baa8d7e0bb7ccc27d12ace579f
Noah Levitt <nlevitt@src.gnome.org>
2003-01-04 04:23:09 +0000

    got the code from gnome-panel

--- src/disclosure-widget.c
+++ src/disclosure-widget.c
@@ -73,27 +73,38 @@ finalize (GObject *object)
 }
 
 static void
+cddb_disclosure_destroy (GtkObject *obj)
+{
+	CDDBDisclosure *disclosure = CDDB_DISCLOSURE (obj);
+
+	if (disclosure->priv->expand_id)
+		g_source_remove (disclosure->priv->expand_id);
+}
+
+static void
 get_x_y (CDDBDisclosure *disclosure,
 	 int *x,
 	 int *y,
 	 GtkStateType *state_type)
 {
 	GtkCheckButton *check_button;
-	GdkRectangle new_area, restrict_area;
 	int indicator_size, indicator_spacing;
 	int focus_width;
 	int focus_pad;
 	gboolean interior_focus;
 	GtkWidget *widget = GTK_WIDGET (disclosure);
-	GtkAllocation *area = &widget->allocation;
 	GtkBin *bin = GTK_BIN (disclosure);
-	GtkRequisition child_requisition;
-	int width, height;
+	int width;
 	
 	if (GTK_WIDGET_VISIBLE (disclosure) &&
 	    GTK_WIDGET_MAPPED (disclosure)) {
 		check_button = GTK_CHECK_BUTTON (disclosure);
 		
+		gtk_widget_style_get (GTK_WIDGET (check_button),
+				      "indicator_size", &indicator_size,
+				      "indicator_spacing", &indicator_spacing,
+				      NULL);
+
 		gtk_widget_style_get (widget,
 				      "interior_focus", &interior_focus,
 				      "focus-line-width", &focus_width,
@@ -107,12 +118,12 @@ get_x_y (CDDBDisclosure *disclosure,
 		}
 
 		if (bin->child) {
-			width = bin->child->allocation.x - widget->allocation.x - (2 * GTK_CONTAINER (widget)->border_width);
+			width = indicator_spacing * 3 + indicator_size ;
 		} else {
-			width = widget->allocation.width;
+			width = widget->allocation.width - 2 * GTK_CONTAINER (widget)->border_width;
 		}
 		
-		*x = widget->allocation.x + (width) / 2;
+		*x = widget->allocation.x + GTK_CONTAINER (widget)->border_width + (width) / 2;
 		*y = widget->allocation.y + widget->allocation.height / 2;
 
 		if (interior_focus == FALSE) {
@@ -122,7 +133,7 @@ get_x_y (CDDBDisclosure *disclosure,
 		*state_type = GTK_WIDGET_STATE (widget) == GTK_STATE_ACTIVE ? GTK_STATE_NORMAL : GTK_WIDGET_STATE (widget);
 
 		if (gtk_widget_get_direction (widget) == GTK_TEXT_DIR_RTL) {
-			*x = widget->allocation.x + widget->allocation.width - (indicator_size + *x - widget->allocation.x);
+			*x = widget->allocation.x + widget->allocation.width - (*x - widget->allocation.x);
 		}
 	} else {
 		*x = 0;
@@ -134,7 +145,6 @@ get_x_y (CDDBDisclosure *disclosure,
 static gboolean
 expand_collapse_timeout (gpointer data)
 {
-	GdkRectangle area;
 	GtkWidget *widget = data;
 	CDDBDisclosure *disclosure = data;
 	GtkStateType state_type;
@@ -163,6 +173,7 @@ expand_collapse_timeout (gpointer data)
 		g_object_set (G_OBJECT (disclosure),
 			      "label", disclosure->priv->hidden,
 			      NULL);
+			      
 		return FALSE;
 	} else if ((int) disclosure->priv->style < (int) GTK_EXPANDER_COLLAPSED) {
 		disclosure->priv->style = GTK_EXPANDER_COLLAPSED;
@@ -186,7 +197,7 @@ do_animation (CDDBDisclosure *disclosure,
 	      gboolean opening)
 {
 	if (disclosure->priv->expand_id > 0) {
-		gtk_timeout_remove (disclosure->priv->expand_id);
+		g_source_remove (disclosure->priv->expand_id);
 	}
 
 	disclosure->priv->direction = opening ? 1 : -1;
@@ -230,6 +241,7 @@ static void
 class_init (CDDBDisclosureClass *klass)
 {
 	GObjectClass *object_class;
+	GtkObjectClass *gtk_object_class = (GtkObjectClass *) klass;
 	GtkWidgetClass *widget_class;
 	GtkCheckButtonClass *button_class;
 	GtkToggleButtonClass *toggle_class;
@@ -243,6 +255,8 @@ class_init (CDDBDisclosureClass *klass)
 	button_class->draw_indicator = draw_indicator;
 
 	object_class->finalize = finalize;
+
+	gtk_object_class->destroy = cddb_disclosure_destroy;
 
 	parent_class = g_type_class_peek_parent (klass);
 
