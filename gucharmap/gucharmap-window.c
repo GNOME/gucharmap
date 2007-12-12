f89972a12410d503dc31f99f9f6ad1a914418d6b
Christian Persch <chpe@gnome.org>
2007-12-12 19:33:26 +0000

    Update FSF address. Bug #407323.
    2007-12-12  Christian Persch  <chpe@gnome.org>
    
    	* *: Update FSF address. Bug #407323.
    
    svn path=/trunk/; revision=1493

--- gucharmap/gucharmap-window.c
+++ gucharmap/gucharmap-window.c
@@ -13,7 +13,7 @@
  *
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, write to the Free Software Foundation, Inc.,
- * 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+ * 59 Temple Place, Suite 330, Boston, MA 02110-1301  USA
  */
 
 #include "config.h"
@@ -391,7 +391,7 @@ help_about (GtkAction       *action,
        "the GNU General Public License and Unicode Copyright for more details."),
     N_("You should have received a copy of the GNU General Public License "
        "along with Gucharmap; if not, write to the Free Software Foundation, Inc., "
-       "59 Temple Place, Suite 330, Boston, MA  02111-1307  USA"),
+       "59 Temple Place, Suite 330, Boston, MA  02110-1301  USA"),
     N_("Also you should have received a copy of the Unicode Copyright along "
        "with Gucharmap; you can always find it at Unicode's website: "
        "http://www.unicode.org/copyright.html")
