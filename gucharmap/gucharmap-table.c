0664b5c84f92238c1f3381d52c924f963259300b
Christian Persch <chpe@gnome.org>
2007-12-25 08:53:35 +0000

    Check for NULL before unref'ing.
    2007-12-25  Christian Persch  <chpe@gnome.org>
    
    	* gucharmap/gucharmap-table.c:
    	(gucharmap_table_set_codepoint_list): Check for NULL before unref'ing.
    
    svn path=/trunk/; revision=1509

--- gucharmap/gucharmap-table.c
+++ gucharmap/gucharmap-table.c
@@ -164,7 +164,8 @@ gucharmap_table_set_codepoint_list (GucharmapTable         *chartable,
                                     GucharmapCodepointList *list)
 {
   gucharmap_chartable_set_codepoint_list (GUCHARMAP_CHARTABLE (chartable->drawing_area), list);
-  g_object_unref (list); /* ABI compat */
+  if (list)
+    g_object_unref (list); /* ABI compat */
 }
 
 GucharmapCodepointList *
