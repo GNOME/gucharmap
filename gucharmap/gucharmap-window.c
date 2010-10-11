be49645c27bb99fde609ae47de5a77e8a62e092a
Christian Persch <chpe@gnome.org>
2010-10-11 12:53:37 +0200

    Unicode 6.0.0

--- gucharmap/gucharmap-window.c
+++ gucharmap/gucharmap-window.c
@@ -486,9 +486,9 @@ help_about (GtkAction       *action,
   gtk_show_about_dialog (GTK_WINDOW (guw),
 			 "program-name", _("GNOME Character Map"),
 			 "version", VERSION,
-			 "comments", _("Based on the Unicode Character Database 5.2"),
+			 "comments", _("Based on the Unicode Character Database 6.0.0"),
 			 "copyright", "Copyright © 2004 Noah Levitt\n"
-				      "Copyright © 1991–2009 Unicode, Inc.\n"
+				      "Copyright © 1991–2010 Unicode, Inc.\n"
 				      "Copyright © 2007–2010 Christian Persch",
 			 "documenters", documenters,
 			 "license", license_trans,
