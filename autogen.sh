24e954c83fb676e44be08074f25ad837f6842907
Christian Persch <chpe@gnome.org>
2013-06-03 19:20:31 +0200

    build: Use autoreconf instead of gnome-autogen

--- autogen.sh
+++ autogen.sh
@@ -1,23 +1,16 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
+set -e
+
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="gucharmap"
+cd "$srcdir"
+mkdir -p m4 >/dev/null 2>&1 || true
+gtkdocize --copy --flavour no-tmpl
+autoreconf --verbose --force --install
+intltoolize --force
+cd -
 
-(test -f $srcdir/gucharmap/main.c) || {
-    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level $PKG_NAME directory"
-    exit 1
-}
-
-which gnome-autogen.sh || {
-    echo "You need to install gnome-common from GNOME git and make"
-    echo "sure the gnome-autogen.sh script is in your \$PATH."
-    exit 1
-}
-
-REQUIRED_INTLTOOL_VERSION=0.40.4
-REQUIRED_AUTOMAKE_VERSION=1.9
-. gnome-autogen.sh
+test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
