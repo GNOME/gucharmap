2ca7ab17e9ad053447f3f05bd45fc545364add51
Christian Persch <chpe@gnome.org>
2013-12-16 18:05:05 +0100

    unicode: First look at version 7

--- gucharmap/gucharmap-window.c
+++ gucharmap/gucharmap-window.c
@@ -446,10 +446,10 @@ help_about (GSimpleAction *action,
   gtk_show_about_dialog (GTK_WINDOW (guw),
 			 "program-name", _("GNOME Character Map"),
 			 "version", VERSION,
-			 "comments", _("Based on the Unicode Character Database 6.3.0"),
+			 "comments", _("Based on the Unicode Character Database 7.0.0"),
 			 "copyright", "Copyright © 2004 Noah Levitt\n"
-				      "Copyright © 1991–2013 Unicode, Inc.\n"
-				      "Copyright © 2007–2012 Christian Persch",
+				      "Copyright © 1991–2014 Unicode, Inc.\n"
+				      "Copyright © 2007–2013 Christian Persch",
 			 "documenters", documenters,
 			 "license", license_trans,
 			 "wrap-license", TRUE,
