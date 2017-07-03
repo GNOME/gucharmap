de1d2aadc44da27d4dd2dc1309f0b404d1a82540
Rafael Fontenelle <rafaelff@gnome.org>
2017-07-03 11:18:11 -0300

    Update FSF postal address
    https://bugzilla.gnome.org/show_bug.cgi?id=721509

--- gucharmap/gucharmap-window.c
+++ gucharmap/gucharmap-window.c
@@ -15,7 +15,7 @@
  *
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, write to the Free Software Foundation, Inc.,
- * 59 Temple Place, Suite 330, Boston, MA 02110-1301  USA
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1335 USA
  */
 
 #include <config.h>
@@ -441,7 +441,7 @@ help_about (GSimpleAction *action,
        "the GNU General Public License and Unicode Copyright for more details."),
     N_("You should have received a copy of the GNU General Public License "
        "along with Gucharmap; if not, write to the Free Software Foundation, Inc., "
-       "59 Temple Place, Suite 330, Boston, MA  02110-1301  USA"),
+       "51 Franklin Street, Fifth Floor, Boston, MA 02110-1335 USA
     N_("Also you should have received a copy of the Unicode Copyright along "
        "with Gucharmap; you can always find it at Unicode's website: "
        "http://www.unicode.org/copyright.html")
