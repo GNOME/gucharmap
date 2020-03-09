33e14bf14c8e6915947fd0138df0fc14da6749d5
Christian Persch <chpe@src.gnome.org>
2020-03-09 20:27:13 +0100

    unicode: Update to version 12.1.0

--- gucharmap/gucharmap-window.c
+++ gucharmap/gucharmap-window.c
@@ -454,7 +454,7 @@ help_about (GSimpleAction *action,
   gtk_show_about_dialog (GTK_WINDOW (guw),
 			 "program-name", _("GNOME Character Map"),
 			 "version", VERSION,
-			 "comments", _("Based on the Unicode Character Database 12.0.0"),
+			 "comments", _("Based on the Unicode Character Database 12.1.0"),
 			 "copyright", "Copyright © 2004 Noah Levitt\n"
 				      "Copyright © 1991–2019 Unicode, Inc.\n"
 				      "Copyright © 2007–2019 Christian Persch\n"
